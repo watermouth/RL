0e398e262a7d0eaf8affac7a041a80dca498f9b1
watermouth <at.imac>
2014-05-22 23:48:50 +0900

    explicit comparison between two step size

--- script/1DTaxiExe.R
+++ script/1DTaxiExe.R
@@ -3,7 +3,8 @@
 #' 素朴にはじっとしていると,コスト関数分だけValueFunctionが悪化する.
 #' 従って, 周りのValueFunctionが良い状態への遷移が促される.
 #' しかし, ValueFunctionの更新具合が遅くなりすぎると,なかなか動かなくなる,ということ.
-x <- Taxi1D(N=1,AS=c(-1,0,1), Cost=Taxi1DCost2)
+x <- Taxi1D(N=1,AS=c(-1,0,1), Cost=Taxi1DCost2, print.state=T)
+x <- Taxi1D(N=1,AS=c(-1,0,1), step.size.fun=function(x){1/sqrt(x)},Cost=Taxi1DCost2, print.state=T)
 
 #' 障害を乗り越える
 #' コスト関数に障害を反映させておかないといつまでたってもだめ。
